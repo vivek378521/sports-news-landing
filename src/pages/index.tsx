8368b72aa94a4d1dd0921e9b4cc7cca56387cfd6
Vivek Khatri <vivek@esamudaay.com>
2023-03-15 17:27:37 +0530

    Use different set of jsx style to make the webpage mobile friendly
    l
    l"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,82 +66,89 @@ export default function LandingPage({ articles }: LandingPageProps) {
 
 
       <style jsx>{`
-       .container {
-        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
-        max-width: 100%;
-        margin: 0 auto;
-        padding: 2rem;
-        min-height: 100vh;
-      }
-      
-      .image-container {
-        position: relative;
-        width: 100%;
-        height: 200px;
-        margin-bottom: 1rem;
-      }
+  .container {
+    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 2rem;
+    min-height: 100vh;
+  }
+  
+  .image-container {
+    position: relative;
+    width: 100%;
+    height: 200px;
+    margin-bottom: 1rem;
+  }
 
-       .articles-container {
-         display: flex;
-         justify-content: space-between;
-         align-items: center;
-         flex-wrap: wrap;
-         gap: 2rem;
-         padding: 2rem;
-         background-color: #f9f9f9;
-       }
-    
-       .column {
-        display: flex;
-        flex-direction: column;
-        gap: 2rem;
-        flex: 1;
-        max-width: calc(50% - 1rem); /* subtract half of the gap */
-      }
-      .article {
-        background-color: #f2f2f2;
-        padding: 1.5rem;
-        border-radius: 0.5rem;
-        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-        max-width: 100%;
-        flex-basis: calc(50% - 1rem);
-      }
+  .articles-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    background-color: #f9f9f9;
+  }
 
+  .column {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    flex: 1;
+    max-width: 100%;
+  }
 
-      h1 {
-        text-align: center;
-        font-size: 2.5rem;
-        margin-bottom: 2rem;
-        color: #1e293b;
-      }
-      
-      h2 {
-        font-size: 1.5rem;
-        margin-bottom: 1rem;
-        color: black;
-        text-shadow: 1px 1px 2px #333;
-      }
-      
+  .article {
+    background-color: #f2f2f2;
+    padding: 1.5rem;
+    border-radius: 0.5rem;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  }
 
-       p {
-         font-size: 1.2rem;
-         color: #4b5563;
-       }
+  h1 {
+    text-align: center;
+    font-size: 2.5rem;
+    margin-bottom: 2rem;
+    color: #1e293b;
+  }
 
+  h2 {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+    color: black;
+    text-shadow: 1px 1px 2px #333;
+  }
 
-       a {
-         display: block;
-         margin-top: 1rem;
-         font-size: 1.2rem;
-         color: #3182ce;
-       }
+  p {
+    font-size: 1.2rem;
+    color: #4b5563;
+  }
 
-       @media (max-width: 768px) {
-        .article {
-          flex-basis: 100%;
-        }
-      }
-     `}</style>
+  a {
+    display: block;
+    margin-top: 1rem;
+    font-size: 1.2rem;
+    color: #3182ce;
+  }
+
+  @media (min-width: 768px) {
+    .articles-container {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
+      padding: 2rem;
+    }
+
+    .column {
+      max-width: calc(50% - 1rem);
+    }
+
+    .article {
+      flex-basis: calc(50% - 1rem);
+    }
+  }
+`}</style>
+
     </div>
   );
 }
@@ -155,10 +162,6 @@ export const getStaticProps: GetStaticProps<LandingPageProps> = async () => {
   });
   const feed = await parser.parseURL('https://www.essentiallysports.com/feed/');
   const articles = feed.items;
-  feed.items.forEach(item => {
-    console.log(item.title);
-    console.log(item['media:content']['$']['url']); // media:content tag url
-  });
   return {
     props: {
       articles,
